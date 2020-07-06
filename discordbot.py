1edd19ff230354614841f2c5d8d27978173e4277
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-07-06 20:18:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,22 @@
 import discord
+import random
+import time
+import discord.ext
+from discord.exe import commands
 import os
+import traceback
 
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+bot = commands.Bot(command_prefix="m:", help_command=None)
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
-client = discord.Client()
-
-# 起動時に動作する処理
-@client.event
+@bot.event
 async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+    print("ログイン完了")
+    
+@bot.command()
+async def test(ctx) :
+    await ctx.send("こん")
+    
+bot.run(token)
